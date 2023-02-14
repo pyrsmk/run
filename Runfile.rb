000cc6cb0e641a37bb6c27bbed7e7e9ea9557b3a
Aurélien Delogu <aurelien.delogu@gmail.com>
2023-02-14 12:10:25 +0100

    Remove gem after publishing

--- Runfile.rb
+++ Runfile.rb
@@ -6,6 +6,7 @@ task :publish do
   gemspec = Gem::Specification::load(GEMSPEC_FILE_PATH)
   shell "gem build #{gemspec.name}"
   shell "gem push #{gemspec.name}-#{gemspec.version}.gem"
+  `rm #{gemspec.name}-#{gemspec.version}.gem`
 end
 
 task :specs do
