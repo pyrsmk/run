98f89283339691939d4bcc05a51cd1e5dd622119
Aurélien Delogu <aurelien.delogu@gmail.com>
2026-04-09 18:50:43 +0200

    Improve pause helper

--- TODO.md
+++ TODO.md
@@ -1,7 +1,3 @@
 # TODO
 
-- ne pas rediriger "run version" vers "run help" et juste afficher la version
-- ajouter des vérifications dans les tâches globales (???)
-
-- mettre un texte à pause
 - complétion automatique dans le terminal
--- src/run/helper/pause_helper.rb
+++ src/run/helper/pause_helper.rb
@@ -3,8 +3,18 @@
 module Run
   module Helper
     class PauseHelper
+      DEFAULT_TEXT = "Press enter to continue"
+
+      # @param text [String]
+      def initialize(text = DEFAULT_TEXT)
+        raise ArgumentError.new("'text' must be a String") if !text.is_a?(String)
+
+        @text = text
+      end
+
       # @return [void]
       def run
+        print "#{@text}..."
         STDIN.gets("\n")
       end
     end
